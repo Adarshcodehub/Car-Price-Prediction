6e85b75fbc16fc848d697d40f491d9424af680a0
AdarshPandey <ap691331@gmail.com>
2024-03-22 19:54:00 +0530

    Initial commit

---
+++ Firstfile.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int a,b[20],i;
+printf("Enter the Number: ");
+scanf("%d %s",&a,b);
+for(i=0;i<20;i++)
+{
+	for(int k=0;i<20;j++)
+	{
+	  printf("%d is the number which is typed by %c",a[k],b[i]);
+    }
+}
+return 0;
+}
---
+++ ProgramC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    //clrscr();
+    printf("This is my First program is Visual; Studio in C Programming");
+    //getch;
+    return 0;
+}
